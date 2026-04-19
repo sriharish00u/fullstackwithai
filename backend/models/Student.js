c5c5e421507d268514a62a1cd39f60f43b80ed52
Jay Harish P <arishexim011@gmail.com>
2026-04-19 11:54:27 +0530

    Delete backend directory

--- backend/models/Student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const studentSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  isDeleted: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model("Student", studentSchema);
